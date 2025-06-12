ad38485f543905c65a407efc210911db875c0117
Maria Eduarda <dudaswc@gmail.com>
2025-06-11 21:30:06 -0300

    Update estilista.py
    Adição da classa Árvore Binária (BinaryTree)

--- estilista.py
+++ estilista.py
@@ -6,3 +6,27 @@ class Node:
 
     def __str__(self):
         return self.content
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+    def getRoot(self):
+        return self.root
+
+    def interactivePath(self, node):
+        if node is None:
+            return
+        print("\n" + node.content)
+        if node.left is None and node.right is None:
+            print("🌟 Fim da jornada.")
+            return
+        choice = input("Digite 'esquerda' ou 'direita': ").strip().lower()
+        if choice == 'esquerda':
+            self.interactivePath(node.left)
+        elif choice == 'direita':
+            self.interactivePath(node.right)
+        else:
+            print("Opção inválida. Tente novamente.")
+            self.interactivePath(node)
+
